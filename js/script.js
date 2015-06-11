b9d89e3e503b96bd9bb1860a4078aeb7604b4182
Marcelo Barros <xavierbarros@gmail.com>
2015-06-11 18:35:13 -0300

    Added periscope effect to the rounded item.

--- js/script.js
+++ js/script.js
@@ -19,4 +19,15 @@ $(window).scroll(function(){
       }, 150 * (i + 1));
     });
   }
+
+  if (wScroll > $('.large-window').offset().top - $(window).height()) {
+    $('.large-window').css({
+      'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'
+    });
+  var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5)
+
+  $('.window-tint').css({
+    'opacity': opacity
+  })
+  }
 });
