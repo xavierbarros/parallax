501101d262e50bacfc7d90e8a8612c3582c7d679
Marcelo Barros <xavierbarros@gmail.com>
2015-06-16 11:27:51 -0300

    Adding blog floating elements to the page.

--- js/script.js
+++ js/script.js
@@ -30,4 +30,16 @@ $(window).scroll(function(){
     'opacity': opacity
   })
   }
+
+  if (wScroll > $('.blog-posts').offset().top - $(window).height()) {
+    var offset =  Math.min(0, wScroll - $('.blog-posts').offset().top + $(window).height() - 350);
+
+    $('.post-1').css({
+      'transform':'translate('+ offset +'px, '+ Math.abs(offset * 0.2) +'px)'
+    });
+
+    $('.post-3').css({
+      'transform':'translate('+ Math.abs(offset) +'px,'+ Math.abs(offset * 0.2) +'px)'
+    });
+  }
 });
